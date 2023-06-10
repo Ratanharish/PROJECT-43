2c950f87dc0672c355ba37582f8174e06bdfebb7
Ratanharish <122155031+Ratanharish@users.noreply.github.com>
2023-06-10 12:05:50 +0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "ARRAY";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R A Y R A", 100,50)
+    fill("lightblue")
+    text("Hint: it stores muliple imformation in one variable !! ,(answer in all capital letters) ", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!,(answer in all capital letters)", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!,(answer in all capital letters)", 100,270)
+
+}
